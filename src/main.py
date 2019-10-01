9d56f866e4561ab217ef88e07d05550c481e2050
Matheus Barbosa <matheusbmiranda@gmail.com>
2019-10-01 19:51:30 -0300

    Comecando uma certa heuristica no algoritmo SAD

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ winsize = 0
 
 def DrawDot(event,x,y,flags,param):
     global contador
+    global img1
     global pixel
     global aux_x
     global aux_y
@@ -28,24 +29,21 @@ def DrawDot(event,x,y,flags,param):
         clone0 = np.copy(img0)
         contador = 1
         clone0[y,x] = [0,255,0]
-        cv2.imshow("fotinha nova", clone0)
+  #    cv2.imshow("fotinha nova", clone0)
 
 
 if __name__ == "__main__":
 
-    img0 = cv2.imread('Piano0.png')
-    img1 = cv2.imread('Piano1.png')
+    img0 = cv2.imread('Piano0.png') #reading left image
 
     img0 = cv2.resize(img0, (1080, 720))
-    img1 = cv2.resize(img1, (1080, 720))
+ #   img1 = cv2.resize(img1, (1080, 720))
 
     print("Digitar numero da janela para algoritmo SAD (numero impar, por favor): ")
     winsize = input()
     winsize = int(winsize)
 
     cv2.imshow("Imagem 0", img0)
-    #cv2.imshow("Imagem 1", img1)
-
 
     cv2.setMouseCallback("Imagem 0", DrawDot)
 
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,33 @@
 import cv2
 import numpy as np
 
+
+#Essa funcao foi feita para preparar as janelas para o algoritmo SAD,
+#Cada janela, com tamanho definido pelo usuario na main, é uma colecao
+#de triplas que contêm um valor RGB. Essa tripla é reduzida a um inteiro
+#que é o módulo do "vetor RGB". Esse valor é utilizado para realizar o SAD.
 def SumAbsoluteDiff(img0, aux_x, aux_y, winsize):
     window = np.zeros((winsize,winsize,3), dtype = np.uint8)
-    for i in range (0, winsize):
-        for j in range (0, winsize):
-            offset_i = i - (winsize%2)
-            offset_j = j - (winsize%2)
-            window[i,j] += img0[aux_y - j , aux_x - i]
+    img1 = cv2.imread('Piano1.png') #lendo imagem da esquerda
+    img1 = cv2.resize(img1, (1080, 720))
+    x0 = aux_x - winsize//2
+    y0 = aux_y - winsize//2
+    for j in range (0, winsize):
+        for i in range (0, winsize):    
+            window[i,j] = img0[y0 + i , x0 + j]
     cv2.imshow("janela", window)
-    
+    aux = img1[300:winsize+300, 300:winsize+300]
+    cv2.imshow("aux", aux)
+
+    absdif1 = np.square(window)
+    absdif1 = np.sum(absdif1)
+    absdif1 = np.sqrt(absdif1)
+    absdif = np.square(aux)
+    absdif = np.sum(absdif)
+    absdif = np.sqrt(absdif)
+    absdif = cv2.absdiff(absdif1, absdif)
+    sumabsdif = np.sum(absdif)
+ #   sumabsdif = np.sum(sumabsdif)
+    print(sumabsdif)
+    #cv2.imshow("outra janela", aux)
     
