cbb8eef0078eee071975d68a2770e0090714f97b
Matheus Barbosa <matheusbmiranda@gmail.com>
2019-10-07 21:19:10 -0300

    mais coisas

--- src/main.py
+++ src/main.py
@@ -9,26 +9,34 @@ pixel = 0
 aux_x = 0
 aux_y = 0
 winsize = 0
+focal_lenght = 2826.171
+baseline = 178.089
 
 
-def DrawDot(event,x,y,flags,param):
+def Clicks(event,x,y,flags,param):
     global contador
     global pixel
     global aux_x
     global aux_y
     if event == cv2.EVENT_LBUTTONDOWN:
         if contador == 1:
-            cv2.destroyWindow("fotinha nova")
             contador = 0
             img0[aux_y,aux_x] = pixel
         aux_x = x
         aux_y = y
-        utils.TemplateMatching(img0, aux_x, aux_y, winsize)
+        coordE = (aux_x, aux_y)
+        print("Coordenadas da esquerda: ", coordE)
+        coordD = utils.TemplateMatching(img0, aux_x, aux_y, winsize)
+        print("Coordenadas da direita: ", coordD)
+        WorldC = utils.WorldCoord(coordE, coordD, baseline, focal_lenght)
+        print("Coordenadas do mundo X, Y, Z = ", WorldC)
+        img1 = cv2.imread("Piano1.png")
+        img1 = cv2.resize(img1, (1080, 720))
         pixel = img0[y,x]        
         clone0 = np.copy(img0)
         contador = 1
         clone0[y,x] = [0,255,0]
-  #    cv2.imshow("fotinha nova", clone0)
+
 
 
 if __name__ == "__main__":
@@ -36,15 +44,15 @@ if __name__ == "__main__":
     img0 = cv2.imread('Piano0.png') #reading left image
 
     img0 = cv2.resize(img0, (1080, 720))
- #   img1 = cv2.resize(img1, (1080, 720))
+    #img1 = cv2.resize(img1, (1080, 720))
 
-    print("Digitar tamanho da janela, em pixels, para algoritmo SAD (numero impar, por favor): ")
+    print("Digitar tamanho da janela, em pixels, para algoritmo TM_CCOEFF_NORMED (numero impar, por favor): ")
     winsize = input()
     winsize = int(winsize)
 
-    cv2.imshow("Imagem 0", img0)
+    cv2.imshow("Imagem da Esquerda", img0)
 
-    cv2.setMouseCallback("Imagem 0", DrawDot)
+    cv2.setMouseCallback("Imagem da Esquerda", Clicks)
 
     n = cv2.waitKey()
     if n == 27 & 0xFF:
--- src/utils.py
+++ src/utils.py
@@ -9,27 +9,34 @@ def TemplateMatching(img0, aux_x, aux_y, winsize):
     img1 = cv2.imread('Piano1.png') #lendo imagem da esquerda
     img1 = cv2.resize(img1, (1080, 720))
 
+    #Criando um objeto janela a partir do valor W dado pelo usuario.
     window = Window(winsize, window, img0)
     window = window.CreateWindow(aux_x, aux_y)
 
     cv2.imshow("janela", window)
     gray_image = img1.copy()
+    gray_image0 = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
     gray_template = window.copy()
     gray_image = cv2.cvtColor(gray_image, cv2.COLOR_BGR2GRAY)
     gray_template = cv2.cvtColor(gray_template, cv2.COLOR_BGR2GRAY)
 
-    #funcoes de matching do Open CV.
+    Disparity(gray_image0, gray_image)
+
+    #funcoes de matching do Open CV. localminimo e localmaximo sao 
+    #os pontos de minimo e maximo. o utilizado aqui eh o maximo por conta
+    #do metodo cv2.TM_CCOEFF_NORMED.
     res = cv2.matchTemplate(gray_image, gray_template, cv2.TM_CCOEFF_NORMED)
     menorvalor, maiorvalor, localminimo, localmaximo = cv2.minMaxLoc(res)
 
-    #localmaximo = localmaximo[::-1]
+    coordD = (localmaximo[0] + winsize//2, localmaximo[1] + winsize//2)
 
     aux = img1.copy()
     coord1 = localmaximo 
     coord2 = (localmaximo[0] + winsize, localmaximo[1] + winsize)  
-    print(coord1, coord2)
     aux = cv2.rectangle(aux, coord1, coord2, [0,255,0], 2)
     cv2.imshow("Correspondencia", aux)
+
+    return coordD
 
 
 def CheckIfOutOfRange(img, x, y, winsize):
@@ -46,27 +53,15 @@ def CheckIfOutOfRange(img, x, y, winsize):
     else:
         return 0
 
-def BorderCenterPoints(img, winsize):
-    height, width, _ = img.shape
-    borderp = []
-    x = winsize//2
-    y = winsize//2
-    for i in range (0, width//winsize):
-        borderp.append((x,y))
-        x = x + winsize
-    x = winsize//2
-    y = y + winsize
-    for i in range (1, height//winsize):
-        borderp.append((x,y))
-        y = y + winsize
-    x = x + winsize
-    for i in range (1, width//winsize):
-        borderp.append((x,y))
-        x = x + winsize
-    y = y - winsize
-    for i in range (1, height//winsize):
-        borderp.append((x,y))
-        y = y - winsize   
-    print(borderp)
+def WorldCoord(coordE, coordD, baseline, focal_lenght):
+    X = baseline*(coordE[0] + coordD[0])/2*(coordE[0] - coordD[0])
+    Y = baseline*(coordE[1] + coordD[1])/2*(coordE[0] - coordD[0])
+    Z = baseline*focal_lenght / (coordE[0] - coordD[0])
 
-    return borderp
+    return (X, Y, Z)
+
+def Disparity(img0, img1):
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(img0,img1)
+
+    cv2.imshow("disp map",disparity)
